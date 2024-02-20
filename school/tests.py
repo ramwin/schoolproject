b2f17b836b0edc5de7ce5bb6a8128a47bc6e9775
Xiang Wang <ramwin@qq.com>
2024-02-20 08:09:53 +0800

    test: test redis db on test

--- school/tests.py
+++ school/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
+from django_redis import get_redis_connection
+
 
 # Create your tests here.
+
+@override_settings(CACHES={
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://localhost:6379/7",
+    }
+})
+class TestSummary(TestCase):
+    def test_redis_db(self):
+        redis: Redis = get_redis_connection("default")
+        self.assertEqual(
+                redis.client_info()["db"],
+                7,
+        )
--- schoolproject/settings.py
+++ schoolproject/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from dotenv import dotenv_values
+
+
+CONFIG = {
+        **dotenv_values(".env.shared"),
+        **dotenv_values(".env"),
+}
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,3 +129,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": f"{CONFIG['REDIS_LOCATION']}",
+    },
+}
