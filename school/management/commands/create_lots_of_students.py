8e0a9f6284700ad284bf137ad068f9c50d45f465
Xiang Wang <ramwin@qq.com>
2024-03-01 23:20:08 +0800

    feat: 创建很多学生

---
+++ school/management/commands/create_lots_of_students.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Xiang Wang <ramwin@qq.com>
+
+
+import time
+import logging
+from faker import Faker
+
+from django.core.management.base import BaseCommand
+
+from school.models import Student
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        faker = Faker("zh-cn")
+        iteration_count = 100
+        bulk_size = 1000
+        LOGGER.info("创建 %d 个学生", iteration_count * bulk_size)
+        start = time.time()
+        for _ in range(iteration_count):
+            students = []
+            for _ in range(bulk_size):
+                students.append(
+                        Student(
+                            name=faker.name()
+                        )
+                )
+            Student.objects.bulk_create(students)
+        end = time.time()
+        LOGGER.info("耗时: %d, ops: %s", end - start, bulk_size * iteration_count / (end-start))
--- schoolproject/settings.py
+++ schoolproject/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'school',
 ]
 
 MIDDLEWARE = [
@@ -83,8 +84,8 @@ WSGI_APPLICATION = 'schoolproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "schoolproject",
     }
 }
 
