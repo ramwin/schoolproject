b02106344e3d71ef45716fb44016f4a42b5791ff
Xiang Wang <ramwin@qq.com>
2024-03-01 23:26:36 +0800

    feat: model

--- schoolproject/settings.py
+++ schoolproject/settings.py
@@ -136,3 +136,79 @@ CACHES = {
         "LOCATION": f"{CONFIG['REDIS_LOCATION']}",
     },
 }
+
+LOG_DIR = BASE_DIR / 'log'
+LOG_DIR.mkdir(exist_ok=True)
+
+DEFAULT_HANDLERS = [
+    'debug_file', 'info_file',
+    'warning_file', 'error_file', 'console'
+]
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('[%(levelname)5s] %(asctime)s %(pathname)s '
+                       '%(funcName)s (line: %(lineno)d)'
+                       '    %(message)s'),
+        },
+        'simple': {
+            'format': '[%(levelname)s] %(module)s:%(lineno)d %(message)s ',
+        },
+    },
+    'handlers': {
+        'error_file': {
+            'level': "ERROR",
+            'class': 'logging.FileHandler',
+            'filename': LOG_DIR / 'error.log',
+            'formatter': 'verbose',
+        },
+        'warning_file': {
+            'level': "WARNING",
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': LOG_DIR / 'warning.log',
+            'maxBytes': 1024 * 1024 * 10,
+            'backupCount': 20,
+            'formatter': 'verbose',
+        },
+        'info_file': {
+            'level': "INFO",
+            'class': 'logging.handlers.RotatingFileHandler',
+            'maxBytes': 1024 * 1024 * 10,
+            'backupCount': 20,
+            'filename': LOG_DIR / 'info.log',
+            'formatter': 'verbose',
+        },
+        'debug_file': {
+            'level': "DEBUG",
+            'class': 'logging.handlers.RotatingFileHandler',
+            'maxBytes': 1024 * 1024 * 10,
+            'backupCount': 20,
+            'filename': LOG_DIR / 'debug.log',
+            'formatter': 'verbose',
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+    },
+    'loggers': {
+        'default': {
+            'handlers': DEFAULT_HANDLERS,
+            'level': "INFO",
+        },
+        'django': {
+            'handlers': DEFAULT_HANDLERS,
+            'level': "INFO",
+        },
+        'testapp': {
+            'handlers': DEFAULT_HANDLERS,
+            'level': "INFO",
+        },
+        'school': {
+            'handlers': DEFAULT_HANDLERS,
+            'level': "INFO",
+        },
+    },
+}
