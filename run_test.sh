9c47d188783cdee0a413e028fcc0291a6fb783b2
Xiang Wang <ramwin@qq.com>
2024-03-10 21:44:19 +0800

    feat: 测试sync_model

---
+++ run_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Xiang Wang(ramwin@qq.com)
+
+python3 manage.py test sync_model
--- schoolproject/settings.py
+++ schoolproject/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'school',
+    "sync_model",
 ]
 
 MIDDLEWARE = [
