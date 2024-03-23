c410e698b389e4fac53d4c8a8b28a070bb023cc6
Xiang Wang <ramwin@qq.com>
2024-03-23 17:10:48 +0800

    feat: django commands test datetime option

--- schoolproject/settings.py
+++ schoolproject/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django_commands",
     'school',
     "sync_model",
 ]
@@ -115,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
