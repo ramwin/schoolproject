a9bad3db595b4f37cc126964dcaddf5b2c51670b
Xiang Wang <ramwin@qq.com>
2024-12-02 20:32:43 +0800

    logging

--- schoolproject/logging_settings.py
+++ schoolproject/logging_settings.py
@@ -5,25 +5,27 @@
 
 import os
 import sys
-
 from pathlib import Path
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 LOG_DIR = BASE_DIR / 'log'
 LOG_DIR.mkdir(exist_ok=True)
-(LOG_DIR / sys.argv[1]).mkdir(exist_ok=True, parents=True)
+
+if sys.argv[0] == 'manage.py':
+    (LOG_DIR / sys.argv[1]).mkdir(exist_ok=True, parents=True)
 
 DEFAULT_HANDLERS = [
-        'debug_file', 'info_file',
-        'warning_file', 'error_file',
+        'info_file',
+        'warning_file',
+        'error_file',
         "console",
         'command_file',
 ]
 handlers = {
     'error_file': {
         'level': "ERROR",
-        'class': 'logging.FileHandler',
+        'class': 'logging.handlers.TimedRotatingFileHandler',
         'filename': LOG_DIR / 'error.log',
         'formatter': 'verbose',
     },
@@ -37,42 +39,35 @@ handlers = {
     },
     'info_file': {
         'level': "INFO",
-        'class': 'logging.handlers.RotatingFileHandler',
-        'maxBytes': 1024 * 1024 * 10,
-        'backupCount': 20,
+        'class': 'logging.handlers.TimedRotatingFileHandler',
+        'when': 'm',
+        'backupCount': 100,
         'filename': LOG_DIR / 'info.log',
         'formatter': 'verbose',
     },
     'command_file': {
         'level': "INFO",
-        'class': 'logging.handlers.RotatingFileHandler',
-        'maxBytes': 1024 * 1024 * 10,
-        'backupCount': 20,
+        'class': 'logging.handlers.TimedRotatingFileHandler',
+        'when': 'm',
+        'backupCount': 100,
         'filename': LOG_DIR / sys.argv[1] / 'info.log',
         'formatter': 'verbose',
     },
     'debug_file': {
         'level': "DEBUG",
-        'class': 'logging.handlers.RotatingFileHandler',
-        'maxBytes': 1024 * 1024 * 10,
-        'backupCount': 20,
+        'class': 'logging.handlers.TimedRotatingFileHandler',
+        'when': 'm',
+        'backupCount': 100,
         'filename': LOG_DIR / 'debug.log',
         'formatter': 'verbose',
     },
-}
-if os.environ.get("TERM") == "xterm-256color":
-    handlers['console'] = {
+    'console': {
         'class': 'colorlog.StreamHandler',
         'level': "INFO",
         'formatter': 'color',
         'stream': sys.stdout,
     }
-else:
-    handlers['console'] = {
-        'class': 'logging.StreamHandler',
-        'formatter': 'verbose',
-        'stream': sys.stdout,
-    }
+}
 loggers = {
     'default': {
         'handlers': DEFAULT_HANDLERS,
@@ -93,6 +88,7 @@ loggers = {
     'django_commands': { 'handlers': DEFAULT_HANDLERS, 'level': "INFO", "propagate": False },
     'school': { 'handlers': DEFAULT_HANDLERS, 'level': "INFO", "propagate": False },
 }
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
