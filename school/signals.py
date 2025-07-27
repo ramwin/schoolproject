4532c2ad36ec7a57a615c9b163a2210ce7b39cb4
Xiang Wang <ramwin@qq.com>
2025-07-27 20:31:21 +0800

    fix: post_save

--- school/signals.py
+++ school/signals.py
@@ -5,7 +5,7 @@
 import logging
 
 from django.db.backends.signals import connection_created
-from django.db.models.signals import post_init
+from django.db.models.signals import post_init, post_save
 
 from .models.base import Tag
 
@@ -13,9 +13,16 @@ from .models.base import Tag
 LOGGER = logging.getLogger(__name__)
 
 
-def console(*args, **kwargs):
+def console(*args, **kwargs) -> None:
     LOGGER.info("args: %s kwargs %s", args, kwargs)
+
+
+def save_tag(instance: Tag, **kwargs) -> None:
+    LOGGER.info("测试一个数据库的数据保存在另外一个数据库是否触发")
+    LOGGER.info("kwargs: %s", kwargs)
+    LOGGER.info("保存了tag: %s", instance)
 
 
 connection_created.connect(console)
 post_init.connect(console, Tag)
+post_save.connect(save_tag, Tag)
--- schoolproject/database_settings.py
+++ schoolproject/database_settings.py
@@ -3,12 +3,16 @@
 # Xiang Wang <ramwin@qq.com>
 
 
+from .settings import CONFIG
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         "NAME": "schoolproject",
         "max_size": 5,
         "CONN_MAX_AGE": 10,
+        "PASSWORD": CONFIG["password"],
         "OPTIONS": {
             # "pool": True,
         },
@@ -16,5 +20,6 @@ DATABASES = {
     "db_02": {
         'ENGINE': 'django.db.backends.postgresql',
         "NAME": "schoolproject_02",
+        "PASSWORD": CONFIG["password"],
     },
 }
